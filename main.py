917c18b63b409f550c909423ca49247cd796cdda
tyydev1 <noobastrox@gmail.com>
2025-11-02 09:30:58 +0700

    Adjust ground

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ while running:
             quit()
 
     screen.blit(skySurface, (0, 0))
-    screen.blit(groundSurface, (0, 250))
+    screen.blit(groundSurface, (0, 300))
 
     pygame.display.update()
     clock.tick(60)
