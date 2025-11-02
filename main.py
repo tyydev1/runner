5b9c15d3d8c0405f50a5c39ac57447e71671b64e
tyydev1 <noobastrox@gmail.com>
2025-11-02 09:36:41 +0700

    Finish session 1: Add text

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
-clock = pygame.time.Clock()
 running = True
+
+clock = pygame.time.Clock()
+testFont = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 skySurface = pygame.image.load('graphics/Sky.png')
 groundSurface = pygame.image.load('graphics/ground.png')
+textSurface = testFont.render("My game", False, 'Black')
 
 def quit():
     pygame.quit()
@@ -22,6 +25,7 @@ while running:
 
     screen.blit(skySurface, (0, 0))
     screen.blit(groundSurface, (0, 300))
+    screen.blit(textSurface, (300, 50))
 
     pygame.display.update()
     clock.tick(60)
